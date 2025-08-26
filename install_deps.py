9e9ccfa274b89c3330b35f79b0dccfa36a8f09f9
Matthew Yang <matthew.yang204@gmail.com>
2025-08-25 17:55:39 -0700

    Use python -m

--- install_deps.py
+++ install_deps.py
@@ -4,5 +4,5 @@ import os
 pyqtdarktheme_wheel = os.path.join("wheels", "pyqtdarktheme-2.1.0-py3-none-any.whl")
 
 # Install dependencies
-os.system("pip install -r requirements.txt")
+os.system("python -m pip install -r requirements.txt")
 os.system(f"pip install {pyqtdarktheme_wheel}")
