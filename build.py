28fef25b6e9cfdae9d584618fa7689ff85bb67ca
Matthew Yang <matthew.yang204@gmail.com>
2025-08-25 20:34:28 -0700

    Add build script

---
+++ build.py
@@ -0,0 +1,28 @@
+import subprocess
+import sys
+import os
+
+
+def run_pyinstaller():
+    try:
+        main_script = os.path.join('schemix', 'main.py')
+
+        # PyInstaller command to build the executable
+        cmd = [
+            'pyinstaller',
+            main_script,
+            '--onedir',  # Create a single folder
+            '-w',  # Makes it windowed
+            '--icon=icon.ico'
+        ]
+
+        # Run PyInstaller
+        subprocess.check_call(cmd)
+
+        print("Build successful.")
+    except Exception as e:
+        print(f"Build failed: {e}")
+
+
+if __name__ == '__main__':
+    run_pyinstaller()
