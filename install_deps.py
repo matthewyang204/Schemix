6a6af9f1f9d31c5672dcad844e02316b012ce6d0
Matthew Yang <matthew.yang204@gmail.com>
2025-08-25 17:53:40 -0700

    Use cross-platform method of getting wheel path

--- install_deps.py
+++ install_deps.py
@@ -1,4 +1,8 @@
 import os
 
+# Get variables
+pyqtdarktheme_wheel = os.path.join("wheels", "pyqtdarktheme-2.1.0-py3-none-any.whl")
+
+# Install dependencies
 os.system("pip install -r requirements.txt")
-os.system("pip install wheels\pyqtdarktheme-2.1.0-py3-none-any.whl")
+os.system(f"pip install {pyqtdarktheme_wheel}")
