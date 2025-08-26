5c22ba1149179e7cd43e4616511920fe6d30a95a
Matthew Yang <matthew.yang204@gmail.com>
2025-08-26 06:56:34 -0700

    Add working installer

---
+++ install.py
@@ -0,0 +1,56 @@
+import platform
+import sys
+
+class platformError(Exception):
+    pass
+
+class linux:
+    def install():
+        import os
+
+        if os.path.isdir("dist") and not os.path.exists("dist/main/main.exe"):
+            print("Found existing build, proceeding to install it...")
+        else:
+            print("ERROR: No build found")
+            print("Building first...")
+            import build
+            build.run_pyinstaller()
+            print("Build complete, installing build...")
+        
+        installdirDefault = "/opt/rohankishore/schemix"
+        installdir = input(f"Select installation directory (default {installdirDefault}, just press enter):")
+        if not installdir:
+            installdir = installdirDefault
+        else:
+            installdir = os.path.expanduser(installdir)
+        username = os.getenv("USER")
+        if username == "root":
+            lnDir = "/usr/local/bin"
+            deskDir = "/usr/share/applications"
+            os.makedirs(lnDir, exist_ok=True)
+            os.makedirs(deskDir, exist_ok=True)
+        else:
+            lnDir = f"/home/{username}/.local/bin"
+            deskDir = f"/home/{username}/.local/share/applications"
+            os.makedirs(lnDir, exist_ok=True)
+            os.makedirs(deskDir, exist_ok=True)
+
+        print(f"Installing to {installdir}...")
+        os.makedirs(installdir, exist_ok=True)
+        os.system(f"cp -r dist/main/* {installdir}/")
+        
+        print("Creating symlinks...")
+        os.system(f"ln -sf {installdir}/main {lnDir}/schemix")
+
+        print("Creating desktop shortcuts...")
+        os.system(f"cp schemix.desktop {deskDir}/schemix.desktop")
+        os.system(f"sed -i 's#@INSTALLDIR@#{installdir}#g' {deskDir}/schemix.desktop")
+
+if platform.system() == "Windows":
+    raise platformError("We have detected that you are using Windows. Please use the Inno Setup script instead.")
+elif platform.system() == "Linux":
+    linux.install()
+elif platform.system() == "Darwin":
+    raise platformError("We have detected that you are using macOS. Please just use the build script to build the program, and then copy the app bundle from dist.")
+else:
+    raise platformError("Unsupported platform detected. There is no installer for this platform.")
