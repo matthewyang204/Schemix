e62e53a23ce7cbe07ca08c82b528bbab6ab4b815
Matthew Yang <matthew.yang204@gmail.com>
2025-09-08 16:30:47 -0700

    Do this as well for MiscWidgets

--- schemix/core/MiscWidgets.py
+++ schemix/core/MiscWidgets.py
@@ -17,10 +17,12 @@ class BoardSelector(QWidget):
         self.setLayout(QVBoxLayout())
         self.layout().addWidget(QLabel("No board found. Create a board to continue."))
         create_button = QPushButton("➕ Create Board")
-        create_button.clicked.connect(self.create_board)
+        create_button.clicked.connect(MainWindow.create_board)
         self.layout().addWidget(create_button)
 
     def create_board(self):
-        board, ok = QInputDialog.getText(self, "Create Board", "Enter board name:")
-        if ok and board:
-            self.create_board_callback(board)
+        boardList = "\n".join(os.path.basename(str(p)) for p in Path(self.base_dir).iterdir())
+        # board, ok = QInputDialog.getText(self, "Create Board", f"{boardList}\n\nEnter board name:")
+        board = simpledialog.askstring("Create or Load Board", f"Existing Boards:\n{boardList}\n\nTo load an existing board, simply enter its exact name. To create a blank/new board, simply enter the name of the new board.\n\nEnter board name:")
+        if board:
+            self.create_board(board)
