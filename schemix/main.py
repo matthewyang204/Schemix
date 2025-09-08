a66a61b7f3e4e980fa7cce778af08eb1177e5a57
Matthew Yang <matthew.yang204@gmail.com>
2025-09-08 09:47:50 -0700

    Clarify

--- schemix/main.py
+++ schemix/main.py
@@ -267,7 +267,7 @@ class MainWindow(QMainWindow):
     def prompt_create_board(self):
         boardList = "\n".join(os.path.basename(str(p)) for p in Path(self.base_dir).iterdir())
         # board, ok = QInputDialog.getText(self, "Create Board", f"{boardList}\n\nEnter board name:")
-        board = simpledialog.askstring("Create Board", f"Existing Boards:\n{boardList}\n\nTo load an existing board, simply enter its exact name.\n\nEnter board name:")
+        board = simpledialog.askstring("Create or Load Board", f"Existing Boards:\n{boardList}\n\nTo load an existing board, simply enter its exact name. To create a blank/new board, simply enter the name of the new board.\n\nEnter board name:")
         if board:
             self.create_board(board)
 
