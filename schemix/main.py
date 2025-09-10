f268f3c38f826f36b56ba0176a3e0f0215231ee0
Matthew Yang <matthew.yang204@gmail.com>
2025-09-09 17:57:26 -0700

    Need the wrapper function to do more than just run the function

--- schemix/main.py
+++ schemix/main.py
@@ -268,18 +268,21 @@ class MainWindow(QMainWindow):
                     chapter_path.unlink()
                     self.load_chapters()
 
-    def prompt_create_board(self):
+    def prompt_create_board(self, base_dir, queue):
         boardList = "\n".join(os.path.basename(str(p)) for p in Path(self.base_dir).iterdir())
         # board, ok = QInputDialog.getText(self, "Create Board", f"{boardList}\n\nEnter board name:")
         board = simpledialog.askstring("Create or Load Board", f"Existing Boards:\n{boardList}\n\nTo load an existing board, simply enter its exact name. To create a blank/new board, simply enter the name of the new board.\n\nEnter board name:")
-        if board:
-            self.create_board(board)
+        queue.put(board)
     
     def prompt_create_board_wrapper(self):
-        prompt_process = multiprocessing.Process(target=self.prompt_create_board)
+        queue = multiprocessing.Queue()
+        prompt_process = multiprocessing.Process(target=self.prompt_create_board, args=(self.base_dir, queue))
         prompt_process.start()
         prompt_process.join()
-    
+        board = queue.get()
+        if board:
+            self.load_board(board) if os.path.exists(os.path.join(self.base_dir, board)) else self.create_board(board)
+        
     def delete_current_board(self):
         if not self.board_dir:
             return
