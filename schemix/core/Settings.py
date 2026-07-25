19dc9c182db22478ccc36428f815ecf0764ffc85
Matthew Yang <matthew.yang204@gmail.com>
2026-07-25 06:55:56 -0700

    Resolve correct path for script directory

--- schemix/core/Settings.py
+++ schemix/core/Settings.py
@@ -27,7 +27,7 @@ def get_appdata_dirs():
         sys.exit(1)
 
     local_app_data = os.path.join(local_app_data, "Schemix")
-    script_dir = os.path.dirname(os.path.abspath(__file__))
+    script_dir = os.path.dirname(os.path.dirname(__file__))
     
     return local_app_data, script_dir
 
